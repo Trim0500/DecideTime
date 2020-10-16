4e7716c1264b0a92692972c3fae4667f9a224197
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2020-10-16 18:33:27 -0400

    Add project files.

---
+++ DecideTime/Classes/DecisionLoop.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecideTime.Classes
+{
+    public class DecisionLoop
+    {
+        string[] determinants = {"class in 10min", "more than 30min to spare", "an assignment due tomorrow", "a quiz tomorrow", "test next week", 
+                                 "assignments due later (as in in 2 or more days)", "notes to revise", "an extracurricular project to work on", "club affairs to tend to"};
+
+        public void DecideLoop()
+        {
+            string ans;
+
+            foreach (string tempDeter in determinants)
+            {
+                Console.Write(string.Format("Do you have {0}? (yes/no : y/n)", tempDeter));
+                ans = Console.ReadLine().ToUpper();
+                while((ans != "YES" && ans != "Y") && (ans != "NO" && ans != "N"))
+                {
+                    Console.Write("Quit being coy! Answer the question!");
+                    ans = Console.ReadLine().ToUpper();
+                }
+                if (tempDeter != determinants[1] && (ans == "YES" || ans == "Y"))
+                {
+                    Console.WriteLine(string.Format("You should do this during your free time: do/prep for {0}", tempDeter));
+                    break;
+                }
+                else if (tempDeter == determinants[1] && (ans == "NO" || ans == "N")) {
+                    Console.WriteLine("Find something to kill some time idk");
+                    break;
+                }
+                else if (tempDeter == determinants[6] && (ans == "NO" || ans == "N"))
+                    Console.WriteLine("You have nothing to do for school!");
+                else if (tempDeter == determinants[8] && (ans == "NO" || ans == "N"))
+                    Console.WriteLine("It's up to you with what you want to do during your free time!");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ DecideTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecideTime.Classes;
+
+namespace DecideTime
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Time to figure out what to do during free time:");
+            Console.WriteLine();
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine();
+
+            DecisionLoop DL = new DecisionLoop();
+
+            DL.DecideLoop();
+
+            Console.ReadKey();
+        }
+    }
+}
